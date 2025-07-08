ae72c51c277b16c1d9130620e759783b0c2d56e9
Your Name <you@example.com>
2025-07-09 01:42:43 +0530

    Configure frontend for production

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api/users';
+const API_URL = (process.env.REACT_APP_API_URL || 'http://localhost:5001') + '/api/users';
 
 const register = (userData) => {
   return axios.post(API_URL + '/register', userData);
